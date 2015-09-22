fefe13a140d21840c3296b48265b3e74c91c2573
eshamster <hamgoostar@gmail.com>
2015-09-22 23:43:01 +0900

    Make an accordion list by AngularJS
    	modified:   static/css/main.css
    	modified:   static/js/test-angular.js
    	modified:   templates/test-angular.lisp

--- static/js/test-angular.js
+++ static/js/test-angular.js
@@ -1,19 +1,28 @@
-function TestControl ($scope, $http) {
-    $scope.test = 100;
-    $scope.items = [];
-    
-    $scope.getTestData = function () {
-        $http({
-            method : 'GET',
-            url : "test.json"
-        }).success(function(data, status, headers, config) {
-            for (var i = 0; i < data.length; i++) {
-                $scope.items.push(data[i]);
-            }
-            console.log(status);
-            console.log(data);
-        }).error(function(data, status, headers, config) {
-            console.log(status);
-        });
-    };
-}
+angular.module('testApp', [])
+    .controller('testController', function ($scope, $http) {
+        $scope.test = 100;
+        $scope.items = [];
+        
+        $scope.getTestData = function () {
+            $http({
+                method : 'GET',
+                url : "test.json"
+            }).success(function(data, status, headers, config) {
+                for (var i = 0; i < data.length; i++) {
+                    $scope.items.push(data[i]);
+                }
+                console.log(status);
+                console.log(data);
+            }).error(function(data, status, headers, config) {
+                console.log(status);
+            });
+        };
+
+        $scope.toggleSelected = function (index) {
+            $scope.selectedId = ($scope.selectedId != index) ? index : -1;
+        };
+
+        $scope.isSelected = function (index) {
+            return $scope.selectedId == index;
+        }
+    });
--- templates/test-angular.lisp
+++ templates/test-angular.lisp
@@ -11,18 +11,19 @@
 
 (defun test-angular-html ()
   (with-markup-to-string
-    (html5 :ng-app ""
+    (html5 :ng-app "testApp"
            (:head
             (:title "Test AngularJS")
-            (:script :src "http://code.angularjs.org/angular-1.0.0rc3.min.js" nil)
+            (:script :src "https://ajax.googleapis.com/ajax/libs/angularjs/1.2.27/angular.min.js" nil)
+            (:script :src "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js" nil)
             (:script :src "js/test-angular.js" nil)
             (:link :rel "stylesheet" :type "text/css" :href "css/main.css"))
            (:body
             (:div (:a :href "/" "Top"))
-            (:div :ng-controller "TestControl"
+            (:div :ng-controller "testController"
                   "{{test}}" (:br)
+                  "selected-id = {{selectedId}}" (:br)
                   (:button :ng-click "getTestData()" "Get test data") (:br)
-                  (:table
-                   (:tr (:th "name") (:th "email"))
-                   (:tr :ng-repeat "item in items" 
-                        (:td "{{item.name}}") (:td "{{item.mail}}"))))))))
+                  (:dl
+                   (:dt :ng-click "toggleSelected($index)" :ng-repeat-start "item in items" "{{item.name}}")
+                   (:dd :ng-class "{'not-selected': !isSelected($index)}" :ng-repeat-end nil "{{item.mail}}")))))))
