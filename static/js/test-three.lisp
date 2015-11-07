34f45403a9c8e6ecec4cd4a31276dd78e7204dfc
eshamster <hamgoostar@gmail.com>
2015-11-07 12:43:55 +0900

    Refactor parenscripts by "ps." macro.
    In this macro, "$scope.item.push" is converted to "(@ $scope item push)", for example.
    This macro has a family macro, "defun.ps" and "defmacro.ps".
    
    	modified:   static/js/test-angular.lisp
    	modified:   static/js/test-three.lisp
    	modified:   static/js/utils.lisp

--- static/js/test-three.lisp
+++ static/js/test-three.lisp
@@ -7,26 +7,25 @@
            :var)
   (:import-from :caveman-sample.js.utils
                 :setf-with
-                :defun+ps
+                :defun.ps
                 :with-import-ps-def))
 (in-package :caveman-sample.js.test-three)
 
 (defmacro+ps three (&rest rest)
   `(@ -t-h-r-e-e ,@rest))
 
-(defun+ps rotate-mesh (mesh)
-  ((@ mesh rotation set)
-   0
-   (+ (@ mesh rotation y) 0.01)
-   (+ (@ mesh rotation z) 0.01))
-  (if is-keydown
-      ((@ mesh position set) 0 0 0)
-      ((@ mesh position set)
-       (+ (@ mesh position x) 0.05)
-       (+ (@ mesh position y) 0.05)
-       0)))
+(defun.ps rotate-mesh (mesh)
+  (with-slots ((rot rotation) (pos position)) mesh
+    (rot.set 0
+             (+ rot.y 0.01)
+             (+ rot.z 0.01))
+    (if is-keydown
+        (pos.set 0 0 0)
+        (pos.set (+ pos.x 0.05)
+                 (+ pos.y 0.05)
+                 0))))
 
-(defun+ps main ()
+(defun.ps main ()
   (let* ((scene (new ((three -scene))))
          (width 600)
          (height 400)
@@ -36,20 +35,20 @@
          (far 1000)
          (camera (new ((three -perspective-camera) fov aspect near far)))
          (renderer (new (three -web-g-l-renderer))))
-    ((@ camera position set) 0 0 50)
-    ((@ renderer set-size) width height)
-    ((@ document body append-child) (@ renderer dom-element))
+    (camera.position.set 0 0 50)
+    (renderer.set-size width height)
+    (document.body.append-child renderer.dom-element)
     (let ((light (new ((three -directional-light) 0xffffff))))
-      ((@ light position set) 0 0.7 0.7)
-      ((@ scene add) light))
+      (light.position.set 0 0.7 0.7)
+      (scene.add light))
     (let* ((geometry (new ((three -cube-geometry) 30 30 30)))
            (material (new ((three -mesh-phong-material) (create :color 0xff0000))))
            (mesh (new ((three -mesh) geometry material))))
-      ((@ scene add) mesh)
+      (scene.add mesh)
       (labels ((render-loop ()
                  (request-animation-frame render-loop)
                  (rotate-mesh mesh)
-                 ((@ renderer render) scene camera)))
+                 (renderer.render scene camera)))
         (render-loop)))))
 
 (defun js-main ()
--- static/js/utils.lisp
+++ static/js/utils.lisp
@@ -8,7 +8,10 @@
   (:export :setf-with
            :load-ps
            :defun+ps
-           :with-import-ps-func))
+           :defun.ps
+           :with-import-ps-func
+           :ps.
+           :defmacro.ps))
 (in-package :caveman-sample.js.utils)
 
 (defmacro+ps setf-with (target &body rest)
@@ -22,15 +25,53 @@
     `(with-slots ,(extract-slots nil rest) ,target
        (setf ,@rest))))
 
+
+(defun replace-dot-sep (elem)
+  (if (symbolp elem)
+      (let ((name (symbol-name elem))
+            (pack-name (package-name (symbol-package elem))))
+        (cond ((and (> (length name) 1)
+                    (string= name "!!" :start1 0 :end1 2))
+               (intern (subseq name 2) pack-name))
+              ((ppcre:scan "\\." name)
+               `(@ ,@(mapcar (lambda (x) (intern x pack-name))
+                                (ppcre:split "\\." name))))
+              (t elem)))
+      elem))
+
+(defun replace-dot-in-tree (tree)
+  (labels ((rec (rest)
+             (let (result)
+               (when rest
+                 (dolist (elem rest)
+                   (push (if (listp elem)
+                             (rec elem)
+                             (replace-dot-sep elem))
+                         result)))
+               (nreverse result))))
+    (rec tree)))
+
+(defmacro ps. (&body body)
+  `(ps ,@(replace-dot-in-tree body)))
+
+(defmacro defmacro.ps (name args &body body)
+  `(defmacro+ps ,name ,args
+     ,@(replace-dot-in-tree body)))
+
+
 (defun intern-ub (sym)
   (intern (format nil "~A_" (symbol-name sym))))
 
 (defmacro defun+ps (name args &body body)
   (let ((name_ (intern-ub name)))
     `(defun ,name_ ()
-       (ps:ps
+       (ps
          (defun ,name ,args
            ,@body)))))
+
+(defmacro defun.ps (name args &body body)
+  `(defun+ps ,name ,args
+     ,@(replace-dot-in-tree body)))
 
 (defun interleave (lst delim)
   (labels ((rec (result rest)
@@ -46,7 +87,7 @@
                                        ps-lst)
                                                              "
 ")
-                                   (ps:ps ,@body)))
+                                   (ps ,@body)))
 
 
 (defun make-js-path (name &key (for-load nil))
@@ -70,6 +111,7 @@
                            :direction :output
                            :if-exists :supersede
                            :if-does-not-exist :create)
+        (format t "(re-)load js: ~A" name)
         (format out (funcall (intern "JS-MAIN"
                                      (string-upcase
                                       (format nil "~A.js.~A"
