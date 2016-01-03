30c360975823d0cdab3ef6bd60c533c0498e27b1
eshamster <hamgoostar@gmail.com>
2016-01-03 15:55:52 +0900

    Add keybord functions

--- static/js/test-ecs-input.lisp
+++ static/js/test-ecs-input.lisp
@@ -14,9 +14,22 @@
 (defvar.ps key-status (make-hash-table))
 
 (defun.ps is-key-down (keyname)
+  "Return if the key is down"
   (let ((value (gethash (keyboard.keyname-to-keycode keyname) key-status)))
     (and (not (null value))
-         (or (eq value :down) (eq value :now-down)))))
+         (or (eq value :down) (eq value :down-now)))))
+
+(defun.ps is-key-down-now (keyname)
+  "Return if the key is down just in this frame"
+  (let ((value (gethash (keyboard.keyname-to-keycode keyname) key-status)))
+    (and (not (null value))
+         (eq value :down-now))))
+
+(defun.ps is-key-up-now (keyname)
+  "Return if the key is down just in this frame"
+  (let ((value (gethash (keyboard.keyname-to-keycode keyname) key-status)))
+    (and (not (null value))
+         (eq value :up-now))))
 
 (defun.ps process-input ()
   (let ((div (document.query-selector "#debug")))
@@ -25,12 +38,15 @@
                (symbol-macrolet ((value (gethash k key-status)))
                  (if v
                      (case value
-                       (:now-down (setf value :down))
+                       (:down-now (setf value :down))
                        (:down)
-                       (t (setf value :now-down)))
+                       (t (setf value :down-now)))
                      (case value
-                       (:now-up (setf value :up))
+                       (:up-now (setf value :up))
                        (:up)
-                       (t (setf value :now-up))))))
+                       (t (setf value :up-now))))))
              keyboard.key-codes)
-    (div.append-child (create-html-element "div" :html (concatenate 'string "Is 'B' pressed: " (is-key-down :b))))))
+    (div.append-child (create-html-element "div" :html (concatenate 'string "Is 'B' pressed: " (is-key-down :b))))
+    (div.append-child (create-html-element "div" :html (concatenate 'string "Is 'B' released: " (not (is-key-down :b)))))
+    (div.append-child (create-html-element "div" :html (concatenate 'string "Is 'B' pressed now: " (is-key-down-now :b))))
+    (div.append-child (create-html-element "div" :html (concatenate 'string "Is 'B' released now: " (is-key-up-now :b))))))
